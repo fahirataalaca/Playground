fc099faef55cc4da512e4d2a664358f7bdccbbe4
fahirataalaca <ata.alaca@hisarschool.k12.tr>
2024-09-05 17:21:36 +0300

    v3

---
+++ PhotoPhramev2/PhotoPhramev2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoPhramev2
+//
+//  Created by Fahir Ata Alaca on 5.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
