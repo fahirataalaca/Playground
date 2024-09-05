7bce3691f7755461fdbc411d49e5e02df993b652
fahirataalaca <ata.alaca@hisarschool.k12.tr>
2024-09-05 13:10:12 +0300

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+var weather="rain"
+if weather=="rain"{
+    print("Please take an umbrella with you")}
+else if weather=="snow"{
+    print("Please take a scraf with you")}
+else if weather=="sunny"{
+    print("Please take sunglasses with you")}
+else{
+    print("Expected weather isn't present")}
+                
+                
+           
