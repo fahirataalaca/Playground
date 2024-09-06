b298dfcd49ab4b7125ae32e4aa6e14ce55976cd2
fahirataalaca <ata.alaca@hisarschool.k12.tr>
2024-09-06 12:45:55 +0300

    v1

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,8 @@
+func areaofrectangle(longside: Int, shortside: Int) -> Int {
+    let multipiclation = longside * shortside
+    return multipiclation
+}
+
+let result = areaofrectangle(longside:30 , shortside:3)
+print("Area of rectangle: \(result)")
+
